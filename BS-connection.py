2d0eb8c8718d3ba3607eed44a0814a224fbe0327
Hector <hektor.mendana@gmail.com>
2025-01-17 13:27:01 +0100

    created transformFeedURL and getFeed

--- BS-connection.py
+++ BS-connection.py
@@ -1,23 +1,35 @@
 from atproto import Client
 from credentials import BLUESKY_USERNAME, BLUESKY_PASSWORD
+from playwright.sync_api import sync_playwright
 
 client = Client()
 client.login(BLUESKY_USERNAME, BLUESKY_PASSWORD)
 
 bluesky_did = client.get_profile(actor="bsky.app")["did"]
 
-data = client.app.bsky.feed.get_feed({
-    'feed': 'at://' + bluesky_did + '/app.bsky.feed.generator/whats-hot',
-    'limit': 5,
-})
+def transformFeedURL(url):
+    with sync_playwright() as p:
+        browser = p.chromium.launch(headless=True)
 
-print(data["feed"][0]["post"]["like_count"])
-print(data["feed"][1]["post"]["like_count"])
-print(data["feed"][2]["post"]["like_count"])
-print(data["feed"][3]["post"]["like_count"])
-print(data["feed"][4]["post"]["like_count"])
+        page = browser.new_page()
+        page.goto(url)
+        page.wait_for_selector('xpath=//div[contains(text(), "@")]', timeout=10000)
 
+        element = page.locator('xpath=//div[contains(text(), "@")]')
 
-def getFeed(feedURL, limit):
+        owner_username = element.text_content().strip()[2:-1]
+        owner_did = client.get_profile(actor=(owner_username))["did"]
+        last_part = url.split("/")[-1]
+
+        uri = 'at://' + owner_did + '/app.bsky.feed.generator/' + last_part
+
+        return uri
+
+def getFeed(uri, limit):
+    content = client.app.bsky.feed.get_feed({
+        'feed': 'at://' + owner_did + '/app.bsky.feed.generator/' + last_part,
+        'limit': 5,
+    })
     
 
+getFeed("https://bsky.app/profile/bsky.app/feed/whats-hot")
