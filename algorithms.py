2cf5a16dc51034db61d034b60b241d1485a6aed4
Hector <hektor.mendana@gmail.com>
2025-01-25 17:53:47 +0100

    Wrote filterContainsWord

--- algorithms.py
+++ algorithms.py
@@ -1,4 +1,30 @@
+from typing import List, Dic
+import Posts
+import re
 
-def filterContainsWord(posts):
+def filterContainsWord(posts: List[Posts.PostObject], wordsFilter: str):
+    wordsList: List[str] = []
+    word: str = ""
+
+    for letter in wordsFilter:
+
+        if re.match(r'^[a-zA-Z-]$', letter):
+            word += letter
+        else:
+            if word:
+                wordsList.append(word)
+                word = "" 
+
+    if word:
+        wordsList.append(word)
+
+    filteredFeed: List = []
+
     for post in posts:
-        post[]
+        if all(word in post.text for word in wordsList):
+            filteredFeed.append(post)
+
+    return filteredFeed
+
+
+    
